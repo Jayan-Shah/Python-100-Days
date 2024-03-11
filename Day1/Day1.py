d31ff22ae8476bc990dda66b205971299e4a0cfc
Jayan <jayan.shah17563@sakec.ac.in>
2024-03-11 13:20:38 +0530

    Add files via upload

---
+++ Day1/Day1.py
@@ -0,0 +1,11 @@
+# Band Name Generator combining your cityname and petname
+print("\n###########################################################################################################\n")
+print("--------------------------------Welcome to Band Name Generator---------------------------------------------")
+print("\n###########################################################################################################\n")
+
+# Input of city name
+cityname=input("Enter your City Name\n")
+
+# Input of pet name
+petname=input("Enter your pet name\n")
+print("Your band name can be: "+cityname+" "+petname)
